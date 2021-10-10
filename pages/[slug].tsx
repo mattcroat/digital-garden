708c78f1f278a9e0236770680887fcf10a123b3b
mattcroat <mattcroat.dev@gmail.com>
2021-10-10 22:04:11 +0200

    feat: Experimental

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,4 +1,5 @@
 import { useMemo } from 'react'
+import Image from 'next/image'
 import { bundleMDXFile } from 'mdx-bundler'
 import { getMDXComponent } from 'mdx-bundler/client'
 import { readdirSync } from 'fs'
@@ -16,6 +17,10 @@ import remarkUnwrapImages from 'remark-unwrap-images'
 
 import type { BundleMDXOptions } from 'mdx-bundler/dist/types'
 
+// experimental
+import visit from 'unist-util-visit'
+import sizeOf from 'image-size'
+
 interface PostProps {
   code: string
   frontmatter: Frontmatter
@@ -28,9 +33,44 @@ interface Frontmatter {
 
 type Context = { params: { slug: string } }
 
+const mdxComponents = {
+  img: ({ src, alt, width, height }) => (
+    <Image
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      loading="lazy"
+      placeholder="blur"
+      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII="
+    />
+  ),
+}
+
+function setImageSize(options) {
+  console.log(options)
+
+  function transform(tree) {
+    visit(tree, 'element', (node, index, parent) => {
+      if (node.tagName === 'img') {
+        const src = node.properties.src
+        const currentDirectory = cwd()
+        const image = `${currentDirectory}/public${src}`
+        const { width, height } = sizeOf(image)
+        node.properties.width = width
+        node.properties.height = height
+      }
+    })
+  }
+
+  return transform
+}
+
 export default function Post({ code, frontmatter }: PostProps) {
+  // avoid recreating the component every render
   const Component = useMemo(() => getMDXComponent(code), [code])
-  return <Component />
+  // @ts-expect-error the types are wrong here
+  return <Component components={mdxComponents} />
 }
 
 export async function getStaticProps(context: Context) {
@@ -58,6 +98,10 @@ export async function getStaticProps(context: Context) {
         remarkTableofContents,
         // remove paragraph around images
         remarkUnwrapImages,
+      ]
+      options.rehypePlugins = [
+        ...(options.rehypePlugins ?? []),
+        [setImageSize, { dir: 'public' }],
       ]
       return options
     },
